417ec1ae676bafafb17a3fef3d8504cf58b3e3b1
vicburk <burklovic@outlook.com>
2025-10-30 22:01:01 -0400

    Fixed documentation.

--- R/get_intersect.R
+++ R/get_intersect.R
@@ -1,14 +1,15 @@
-#` Get the intersection of a list of sets
-#`
-#` @param sets Either a list of matrices with named rows and columns, or a list of named vectors
-#` @param type The type of sets that you want to get the intersection of. Either "row", "col", or "vector"
-#` @return A vector of the intersection of the sets
-#` @examples
-#`
-#` s <- list(c("a","b","c","d"), c("f","e","d","c"))
-#` get_intersect(s, type = "vector")
-#`
-# Function to find the intersection
+#' Get the intersection of a list of sets
+#'
+#' @param sets Either a list of matrices with named rows and columns, or a list of named vectors
+#' @param type The type of sets that you want to get the intersection of. Either "row", "col", or "vector"
+#'
+#' @return A vector of the intersection of the sets
+#' @export
+#'
+#' @examples
+#' s <- list(c("a","b","c","d"), c("f","e","d","c"))
+#' get_intersect(s, type = "vector")
+#'
 get_intersect <- function(sets, type = c("row", "col", "vector")) {
   # This will throw an error if the type is not valid
   type <- match.arg(type)
